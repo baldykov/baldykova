b72277fc3871cb3be6b3a683eef86aa6a1d00fdb
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-05-15 00:10:11 +0300

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "CFPAT-_KaQA8QilG3ottDPn0XsGgHd6-UP43kg-szhwGaUcuM",
-        spaceId: "",
+        accessToken: "ZdjyW8meJpzNZ-ldhVMB6EJaE_VoSh-xaryOzJ_LHjs",
+        spaceId: "be9hkzkklpmg",
       },
     },
     "gatsby-plugin-emotion",
